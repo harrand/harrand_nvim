106c6772eaa755dc660c3ca37419be0fa87cbdd8
harrand <harrandev@gmail.com>
2024-12-03 04:20:34 +0000

    [plugin] treesitter

--- lua/harrand/packer.lua
+++ lua/harrand/packer.lua
@@ -1,12 +1,14 @@
 
 return require('packer').startup(function(use)
   -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+	use 'wbthomason/packer.nvim'
 
-use {
-  'nvim-telescope/telescope.nvim', tag = '0.1.8',
--- or                            , branch = '0.1.x',
-  requires = { {'nvim-lua/plenary.nvim'} }
-}
+	use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.8',
+	-- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 
 end)
