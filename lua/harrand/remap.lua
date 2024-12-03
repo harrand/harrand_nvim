fda8c4f127cf5b734983f0538581d0ac781007d0
harrand <harrandev@gmail.com>
2024-12-03 04:08:16 +0000

    [plugin] telescope

--- lua/harrand/remap.lua
+++ lua/harrand/remap.lua
@@ -1,3 +1,10 @@
 vim.g.mapleader = " "
 -- Open the directory of the current file in netrw
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Telescope find files in git repo' })
+vim.keymap.set("n", "<leader>fw", function()
+	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
