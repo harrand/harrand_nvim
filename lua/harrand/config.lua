1a01f879fa21696e41435beae1f351c426a5fe8f
harrand <harrandev@gmail.com>
2024-12-03 03:27:30 +0000

    automatically git clone packer on first time use

---
+++ lua/harrand/config.lua
@@ -0,0 +1,7 @@
+-- $HINIT expands to the directory containing this config file.
+vim.fn.setenv("HINIT", vim.fn.stdpath("config"))
+
+vim.opt.expandtab = false
+vim.opt.autoindent = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
--- lua/harrand/init.lua
+++ lua/harrand/init.lua
@@ -1,1 +1,33 @@
-print("hello from harrand")
+require("harrand.config")
+require("harrand.remap")
+
+local packpath = vim.o.packpath
+local first_path = packpath:match("([^,]+)")  -- This matches everything before the first comma
+print(first_path)
+
+local on_first_install = function()
+	-- this code is ran once ever.
+	local firstfile = io.open(first_path .. "/first", "w")
+	firstfile:write()
+	firstfile:close()
+
+	-- todo: stuff that runs the first time this config is used
+	print("Fetching packer...")
+	local job = vim.fn.jobstart(
+		"git clone https://github.com/wbthomason/packer.nvim",
+		{
+			cwd = first_path,
+			on_exit = function() print("Finished fetching packer") end
+		}
+	)
+end
+
+-- First time install
+local firstfile = io.open(first_path .. "/first", "r")
+if firstfile ~= nil then
+	io.close(firstfile)
+	-- not the first time
+else
+	-- first time
+	on_first_install()
+end
