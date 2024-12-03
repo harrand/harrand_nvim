8917134ad3d365dd3b3963fcd195388d3e3eb8f5
harrand <harrandev@gmail.com>
2024-12-03 17:35:23 +0000

    [config] helpful comments in config.lua

--- lua/harrand/config.lua
+++ lua/harrand/config.lua
@@ -1,17 +1,20 @@
 -- $HINIT expands to the directory containing this config file.
 vim.fn.setenv("HINIT", vim.fn.stdpath("config"))
 
+-- tabs are size 4, never replace with spaces. tab supremacy.
 vim.opt.expandtab = false
 vim.opt.autoindent = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 
+-- line numbers enabled and relative
 vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 4
 
 vim.opt.wrap = false
 
+-- when you open an actual file, :cd to its parent directory so cwd is always relevant
 vim.api.nvim_create_autocmd("BufEnter", {
   pattern = "*",
   callback = function()
